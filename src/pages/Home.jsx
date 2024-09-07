706cbe4d5caf0c017e1ccad02ed7e3c789a05e87
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-09-07 22:23:49 +0545

    Added header

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BannerSection } from "../sections/home/BannerSection";
+
+const Home = () => {
+  return (
+    <>
+      <BannerSection />
+    </>
+  );
+};
+
+export default Home;
